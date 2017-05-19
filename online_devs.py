853979c0ca9223953d23fd98e7b9fcafa48e7f60
Gabriel Oliveira <gabriel.oliveira@evoluservices.com>
2017-05-19 17:17:44 -0300

    Refresh the developers list on each iteration
    Signed-off-by: Gabriel Oliveira <gabriel.oliveira@evoluservices.com>

--- online_devs.py
+++ online_devs.py
@@ -3,25 +3,25 @@ import csv
 import requests
 import os
 
-url = os.getenv('REVIEWERS_URL')
-response = requests.get(url)
-data = response.content
-reader = csv.reader(data.splitlines(), delimiter=',')
-
-devs = {dev[2]: dev[1] for dev in list(reader)}
 ps = PortScanner()
 
-def is_dev(ip):
+def is_dev(ip, devs):
     mac = ps[ip]['addresses'].get('mac', 'SEM MAC')
     return mac in devs
 
 
-def get_dev_id(ip):
+def get_dev_id(ip, devs):
     return int(devs[ps[ip]['addresses']['mac']])
 
 
 def get_devs_online():
     ps.scan(hosts='192.168.0.*', arguments='-sP')
-    dev_ids = map(get_dev_id, list(filter(is_dev, ps.all_hosts())))
+    url = os.getenv('REVIEWERS_URL')
+    response = requests.get(url)
+    data = response.content
+    reader = csv.reader(data.splitlines(), delimiter=',')
+
+    devs = {dev[2]: dev[1] for dev in list(reader)}
+    dev_ids = map(lambda x: get_dev_id(x, devs), list(filter(lambda x: is_dev(x, devs), ps.all_hosts())))
     return list(set(dev_ids))
 
